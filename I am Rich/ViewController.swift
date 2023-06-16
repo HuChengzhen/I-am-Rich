63dd4303cd1ab072a42ef70918178c2f5fe10b67
Hu Chengzhen <huchengzhen@gmail.com>
2023-06-17 00:40:12 +0800

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 胡诚真 on 2023/6/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
